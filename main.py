fbbc80c98a459721733e96f7fd4f074e669ee29d
gstrdms <86445295+gstrdms@users.noreply.github.com>
2022-07-25 02:26:35 +0000

    Playing the audio file using Python

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import requests
+from replit import audio
+import time
 
-url = " https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Oddio_Overplay/MIT_Concert_Choir/Carmina_Burana/MIT_Concert_Choir_-_01_-_O_Fortuna.mp3"
-
-r = requests.get(url)
-with open("o_fortuna.mp3", "wb") as f:
-    f.write(r.content)
+audio.play_file("o_fortuna.mp3")
+choice = input("Press enter to stop the music. ")
